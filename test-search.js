46f17bd8703bd8dce8057d47fe6afa66b34bcf39
thotasrinath <srinath.thota@outlook.com>
2023-03-02 15:19:55 +0800

    search test

--- test-search.js
+++ test-search.js
@@ -1,4 +1,5 @@
 import xk6_mongo from 'k6/x/mongo';
+import { SharedArray } from 'k6/data';
 
 
 /**
@@ -7,10 +8,29 @@ import xk6_mongo from 'k6/x/mongo';
  */
 
 const client = xk6_mongo.newClient('mongodb://172.17.0.2:27017');
+
+const data = new SharedArray('words', function () {
+    // All heavy work (opening and processing big files for example) should be done inside here.
+    // This way it will happen only once and the result will be shared between all VUs, saving time and memory.
+    const f = JSON.parse(open('./words_dictionary.json'));
+    return Object.keys(f); // f must be an array
+});
+
+
+function sentenceGenerator(words, size) {
+    var sentence = '';
+    for (var i = 0; i < size; i++) {
+        sentence += words[Math.floor(Math.random() * words.length)] + ' ';
+    }
+
+    return sentence;
+}
+
+
 export default () => {
 
 
-    var query = { $text: { $search: "acetabularia" } };
+    var query = { $text: { $search: sentenceGenerator(data, 5) } };
 
     console.log("query is :", query);
 
